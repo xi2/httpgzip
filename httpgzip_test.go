f5d8d6dc8fbe9c2d518e5d5cb3969df291ef46c0
Michael Cross <mc@xi2.org>
2015-10-01 16:52:28 +0100

    Relicense httpgzip from GPLv3 to Expat-style MIT License
    The reasons for relicensing are as follows:
    
      (1) httpgzip is in a fairly trivial library. Anyone unwilling to use
          GPL code could probably rewrite it themselves in an
          afternoon. Therefore the GPL license of httpgzip was unlikely to
          result in anyone writing any new GPL software or preventing any
          malicious embedded product from seeing the light of day, which
          were the aims of its use.
    
      (2) The Go community overwhelmingly favours BSD/MIT licenses at
          present. While this alone is not a reason to relicense httpgzip,
          combined with (1) the Expat license seems a better choice due to
          its wider acceptance in the community.

--- httpgzip_test.go
+++ httpgzip_test.go
@@ -1,23 +1,6 @@
-/*
-   Copyright 2015 The Httpgzip Authors. See the AUTHORS file at the
-   top-level directory of this distribution and at
-   <https://xi2.org/x/httpgzip/m/AUTHORS>.
-
-   This file is part of Httpgzip.
-
-   Httpgzip is free software: you can redistribute it and/or modify it
-   under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-
-   Httpgzip is distributed in the hope that it will be useful, but
-   WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with Httpgzip.  If not, see <https://www.gnu.org/licenses/>.
-*/
+// Copyright (c) 2015 The Httpgzip Authors.
+// Use of this source code is governed by a Expat-style
+// MIT license that can be found in the LICENSE file.
 
 package httpgzip_test
 
